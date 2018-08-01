848349fa0728ab43c7a7d28d83737a88edb8b2f7
Scott Weinstein <sweinstein27@gmail.com>
2018-08-01 13:40:26 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,40 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  class Student
+  attr_reader :name, :grade
+
+ STUDENTS = []
+
+  def initialize(params)
+    @name = params[:name]
+    @grade = params[:grade]
+    STUDENTS << self
+  end
+
+  def self.all
+    STUDENTS
+  end
+
+end
+
+class Course
+  attr_reader :name, :topic
+
+  COURSES = []
+
+  def initialize(args)
+    @name = args[:name]
+    @topic = args[:topic]
+    COURSES << self
+  end
+
+  def self.all
+    COURSES
+  end
+end
+
+
+
+
 end
